8eabee78674496b4d5c812cc36c0d75264b1047e
HellooYing <15642330153@163.com>
2018-01-18 14:26:32 +0800

    完成了路径显示,下一步是添加标注注明时间

--- web/views.py
+++ web/views.py
@@ -17,8 +17,6 @@ def change_route(request):
     for i in car:
         point=i.point.split('^')
         point_time=i.point_time.split("^")
-    print(point)
-    print(point_time)
     response_data={'point':point,'point_time':point_time}
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
