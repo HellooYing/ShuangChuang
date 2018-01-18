0881609971422c3c9d8cbe0916ed16582c9e534d
HellooYing <15642330153@163.com>
2018-01-18 13:08:43 +0800

    json数据传递完成,准备危险的修改数据库

--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^test',views.test,name='test'),
+    url(r'^change_route',views.change_route,name='change_route'),
 ]
--- web/views.py
+++ web/views.py
@@ -9,5 +9,18 @@ def index(request):
     AllCar=Car.objects.all()
     return render(request, "index.html",context={'AllCar':AllCar})
 
+def change_route(request):
+    num=request.GET.get('num')
+    car = Car.objects.filter(plate_number=num)
+    point=[]
+    point_time=[]
+    for i in car:
+        point=i.point.split('^')
+        point_time=i.point_time.split("^")
+    print(point)
+    print(point_time)
+    response_data={'point':point,'point_time':point_time}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
 def test(request):
     return render(request, "test.html",context={})
