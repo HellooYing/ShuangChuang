48e14490e2cd451e931c089e0fc5b5139dfdc5e9
hellooying <15642330153@163.com>
2018-09-29 17:02:55 +0800

    目标识别部分目前效果不错

--- pi/getimg/SearchVehical.py
+++ pi/getimg/SearchVehical.py
@@ -1,18 +1,30 @@
-from pipeline import search_car
 import cv2
 import os
-def SaveVehical(VideoPath=r'../video/test1.mp4'):
-    #读取视频
+def SearchVehical(VideoPath):
     cap=cv2.VideoCapture(VideoPath)
-    frameCount=0
+    capture=r".\capture"
+    classIdentify_car=".\cars.xml"
+    classfier = cv2.CascadeClassifier(classIdentify_car)
+    value,frame=cap.read()
+    if not os.path.exists(capture):
+        os.makedirs(capture)
+    FrameCount=0
     while True:
-        frameCount=frameCount+1
-        ret,frame =cap.read()
-        if not ret:
+        FrameCount =FrameCount+1
+        value,frame=cap.read()
+        if not value:
             break
-        #return 窗口，和车辆存储路径
-        CapturePath =search_car(frame,frameCount)
-    return CapturePath
-
+        grey=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        carRects=classfier.detectMultiScale(grey,scaleFactor=1.2,minNeighbors=3,minSize=(64,64))
+        i=0
+        if len(carRects)>0:
+            for carRect in carRects:  # 单独框出每一张车辆
+                x, y, w, h = carRect
+                i=i+1
+                fragment = frame[x:x + w, y:y + h]
+                #cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0), 2)
+                cv2.imwrite(r".\capture\Vehical{}_{}.jpg".format(FrameCount,i),fragment)
+                print("成功捕捉   .\capture\Vehical{}_{}.jpg".format(FrameCount,i))
+    return capture
 if __name__=="__main__":
-    SaveVehical()
+    SearchVehical(r"../video/test1.mp4")
--- pi/pi.py
+++ pi/pi.py
@@ -4,7 +4,7 @@ sys.path.append(r"./kind")
 import cv2
 import color.testColor,color.colorDictionary
 import kind.getkind
-a=kind.getkind.car_type("./img/2.png")
-frame = cv2.imread('./img/2.png')
+a=kind.getkind.car_type("./img/1.png")
+frame = cv2.imread('./img/1.png')
 b=color.testColor.getColorOfFrame(frame)
 print(a,b)
